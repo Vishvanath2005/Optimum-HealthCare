de1d5879b5e1f7bacc76a96e8233780684dea035
Vishvanath2005 <vishva202005@gmail.com>
2025-04-23 15:57:48 +0530

    layout_icons

--- ohc/src/pages/layout/Layout.jsx
+++ ohc/src/pages/layout/Layout.jsx
@@ -1,12 +1,22 @@
 import React, { Suspense, useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
-import { LuLayoutDashboard } from "react-icons/lu";
 import { IoMdMenu } from "react-icons/io";
+import {
+  LayoutDashboard,
+  FileWarning,
+  Receipt ,
+  Tag,
+  CalendarClock,
+  FileBarChart,
+  Wallet,
+  Settings,
+} from "lucide-react";
+import { File, AlertTriangle } from "lucide-react";
+import { Clipboard, DollarSign } from "lucide-react";
 import Header from "./Header";
-import Logo from "../../assets/images/img.png"
+import Logo from "../../assets/images/img.png";
 import Logo_L from "../../assets/images/Logo(light).png";
 import Logo_D from "../../assets/images/Logo(dark).png";
-
 
 const Layout = () => {
   const [open, setOpen] = useState(false);
@@ -14,17 +24,35 @@ const Layout = () => {
   const Menus = [
     {
       title: "Dashboard",
-      icon: <LuLayoutDashboard size={23} />,
+      icon: <LayoutDashboard size={23} />,
       to: "/dashboard",
     },
-    { title: "GMD", icon: <LuLayoutDashboard size={23} />, to: "#" },
-    { title: "Leads", icon: <LuLayoutDashboard size={23} />, to: "#" },
-    { title: "CMS", icon: <LuLayoutDashboard size={23} />, to: "#" },
-    { title: "Appoinment", icon: <LuLayoutDashboard size={23} />, to: "#" },
-    { title: "Finance", icon: <LuLayoutDashboard size={23} />, to: "#" },
-    { title: "Reports", icon: <LuLayoutDashboard size={23} />, to: "#" },
-    { title: "Subscription", icon: <LuLayoutDashboard size={23} />, to: "#" },
-    { title: "Settings", icon: <LuLayoutDashboard size={23} />, to: "#" },
+    { title: "GMD", icon: <FileWarning size={23} />, to: "#" },
+    {
+      title: "Leads",
+      icon: (
+        <div className="relative w-6 h-6">
+      <File className="absolute  w-6 h-6" />
+      <AlertTriangle className="absolute left-1.5 top-2  w-3 h-3" />
+    </div>
+      ),
+      to: "#",
+    },
+    { title: "CMS", icon: <Receipt  size={23} />, to: "#" },
+    { title: "Appoinment", icon: <CalendarClock size={23} />, to: "#" },
+    {
+      title: "Finance",
+      icon: (
+        <div className="relative w-6 h-6">
+          <Clipboard className="absolute " />
+          <DollarSign className="absolute left-1 top-2 w-3 h-3" />
+        </div>
+      ),
+      to: "#",
+    },
+    { title: "Reports", icon: <FileBarChart size={23} />, to: "#" },
+    { title: "Subscription", icon: <Wallet size={23} />, to: "#" },
+    { title: "Settings", icon: <Settings size={23} />, to: "#" },
   ];
 
   return (
@@ -32,28 +60,42 @@ const Layout = () => {
       <div className="w-screen h-screen flex relative sm:static  bg-overall_bg-light dark:bg-overall_bg-dark">
         <div
           className={`${
-            !open ? `sm:static absolute sm:w-[320px] w-4/5 z-10` : `sm:w-28 hidden `
+            !open
+              ? `sm:static absolute sm:w-[320px] w-4/5 z-10`
+              : `sm:w-28 hidden `
           }  h-screen  sm:block dark:bg-layout-dark bg-layout-light`}
         >
-          <div className={`${!open?`pl-3 pt-3`:` pt-6 `}`}
-          onClick={() => setOpen(!open)}>
-            <IoMdMenu
-            size={28}
-              className={`${!open?`text-2xl text-layout_text-light dark:text-layout_text-dark`:`hidden`}`}
-            />
-          
-          <div className="flex justify-center w-full  items-center pb-3  "
+          <div
+            className={`${!open ? `pl-3 pt-3` : ` pt-6 `}`}
+            onClick={() => setOpen(!open)}
           >
-          {!open?(
-              <>
-              <img src={Logo_L} alt="logo" className="w-40 dark:hidden -ml-6"/>
-              <img src={Logo_D} alt="logo" className="hidden w-40 dark:block -ml-6" />
-              </>
-         ):(
-          <img src={Logo} alt="logo" className="w-20"/>
+            <IoMdMenu
+              size={28}
+              className={`${
+                !open
+                  ? `text-2xl text-layout_text-light dark:text-layout_text-dark`
+                  : `hidden`
+              }`}
+            />
 
-         )}
-          </div>
+            <div className="flex justify-center w-full  items-center pb-3  ">
+              {!open ? (
+                <>
+                  <img
+                    src={Logo_L}
+                    alt="logo"
+                    className="w-40 dark:hidden -ml-6"
+                  />
+                  <img
+                    src={Logo_D}
+                    alt="logo"
+                    className="hidden w-40 dark:block -ml-6"
+                  />
+                </>
+              ) : (
+                <img src={Logo} alt="logo" className="w-20" />
+              )}
+            </div>
           </div>
           <ul>
             {Menus.map((menu, index) => (
@@ -63,14 +105,18 @@ const Layout = () => {
                     <span className="text-layout_text-light bg-overall_bg-light dark:bg-overall_bg-dark dark:text-layout_text-dark p-2 rounded-md ">
                       {menu.icon}
                     </span>
-                    <p className={`${open && `hidden`} font-layout-font text-xl`}>{menu.title}</p>
+                    <p
+                      className={`${open && `hidden`} font-layout-font text-xl`}
+                    >
+                      {menu.title}
+                    </p>
                   </li>
                 </NavLink>
               </React.Fragment>
             ))}
           </ul>
         </div>
-        <div className={`${!open ?`sm:p-4`: `sm:p-8 px-1.5`} w-screen`}>
+        <div className={`${!open ? `sm:p-4` : `sm:p-4 px-1.5`} w-screen`}>
           <Header open={open} setOpen={setOpen} />
           <Suspense>
             <Outlet />
