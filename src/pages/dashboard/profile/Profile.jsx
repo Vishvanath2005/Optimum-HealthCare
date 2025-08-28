e8eed215e81dbba11be91803246a1b4b61e97fa1
Vishvanath2005 <vishva202005@gmail.com>
2025-08-28 15:09:15 +0530

    WFH_Leaves

--- src/pages/dashboard/profile/Profile.jsx
+++ src/pages/dashboard/profile/Profile.jsx
@@ -8,9 +8,9 @@ import Edit_Profile from "./Edit_Profile";
 import Dropdown from "../../../component/Dropdown";
 import { Navigate, useNavigate } from "react-router-dom";
 import Change_Password from "./Change_Password";
+import WFH_Leave from "./wfh_leave/WFH_Leave";
 const Profile = () => {
   const navigate = useNavigate();
-  const [apply_Leave, setApplyLeave] = useState(false);
   const [editprofile, setEditprofile] = useState(false);
   const [changepassword, setChangepassword] = useState(false);
 
@@ -26,25 +26,6 @@ const Profile = () => {
     { label: "Language", key: "language" },
     { label: "Reporting Person", key: "reportingPerson" },
     { label: "Address", key: "address" },
-  ];
-
-  const leaves = [
-    {
-      sno: 1,
-      dateApplied: "12 Aug 2025",
-      leaveType: "Sick",
-      duration: "2 Days",
-      reason: "Fever",
-      status: "Approved",
-    },
-    {
-      sno: 2,
-      dateApplied: "12 Aug 2025",
-      leaveType: "Sick",
-      duration: "2 Days",
-      reason: "Fever",
-      status: "Approved",
-    },
   ];
 
    const dropdownItems = [
@@ -65,14 +46,6 @@ const Profile = () => {
   return (
     <div>
       <NavBar title="Dashboard" pagetitle="Profile" />
-      <div className="justify-end flex mx-2">
-        <p
-        onClick={()=>{setApplyLeave(true)}} 
-        className="cursor-pointer w-fit flex items-center  text-white gap-2 bg-select_layout-dark px-4 py-2 text-sm rounded-md">
-          <ClipboardPenLine size={18} />
-          Apply Leave
-        </p>
-      </div>
       <div className="lg:grid grid-cols-12 flex flex-col gap-3 py-3">
         <div className="lg:col-span-5 flex flex-col gap-3  rounded-lg text-white">
           <div className="flex justify-between lg:mb-2  dark:bg-layout-dark bg-layout-light text-black dark:text-white rounded-lg p-4">
@@ -110,46 +83,11 @@ const Profile = () => {
             </div>
           </div>
         </div>
-        <div className="lg:col-span-7 md:grid-cols-1 dark:bg-layout-dark bg-layout-light h-fit p-4 rounded-xl dark:text-white text-black">
-          <p className="text-xl font-semibold pb-3"> My Leaves</p>
-          <div className="overflow-x-auto">
-            <table className="w-full text-sm border-collapse">
-              <thead>
-                <tr className="dark:bg-overall_bg-dark bg-overall_bg-light border-b-2 pb-2 dark:border-layout-dark border-layout-light ">
-                  <th className="px-2 py-3  rounded-l-md">S.No</th>
-                  <th className="p-2">Date Applied</th>
-                  <th className="p-2">Leave Type</th>
-                  <th className="p-2">Duration</th>
-                  <th className="p-2">Reason</th>
-                  <th className="p-2 rounded-r-md">Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {leaves.map((leave, idx) => (
-                  <tr
-                    key={idx}
-                    className="dark:bg-overall_bg-dark bg-overall_bg-light border-b-2 dark:border-layout-dark border-layout-light rounded-lg text-center"
-                  >
-                    <td className="px-2 py-3 text-sm rounded-l-md">
-                      {leave.sno}
-                    </td>
-                    <td className="p-2">{leave.dateApplied}</td>
-                    <td className="p-2">{leave.leaveType}</td>
-                    <td className="p-2">{leave.duration}</td>
-                    <td className="p-2">{leave.reason}</td>
-                    <td className="p-2 text-sm w-32 px-5 items-center rounded-r-md">
-                      <p className="border w-28 dark:bg-layout-dark bg-layout-light text-gray-600 dark:text-white font-medium  dark:border-layout-dark border-layout-light rounded-md py-1">
-                        {leave.status}
-                      </p>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+        <div className="lg:col-span-7 md:grid-cols-1 dark:bg-layout-dark bg-layout-light h-fit px-4 py-2 rounded-xl dark:text-white text-black">
+        <WFH_Leave />
         </div>
       </div>
-      {apply_Leave && (<Apply_Leave onclose={()=>{setApplyLeave(false)}}/>)}
+     
       {editprofile && (<Edit_Profile onclose={()=>{setEditprofile(false)}}/>)}
       {changepassword && (<Change_Password onclose={()=>{setChangepassword(false)}}/>)}
     </div>
