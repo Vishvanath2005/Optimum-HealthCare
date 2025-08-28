f0342e3fb2644448a8ed264d2201654ddf7b22c6
Vishvanath2005 <vishva202005@gmail.com>
2025-08-28 13:00:59 +0530

    WFH in HR role

--- src/pages/hr/Hr.jsx
+++ src/pages/hr/Hr.jsx
@@ -6,6 +6,7 @@ import Employee from "./employee/Employee";
 import Attendance from "./attendance/Attendance";
 import Leave from "./leave/Leave";
 import PayRoll from "./payroll/PayRoll";
+import WFH from "./wfh/WFH";
 
 const Hr = () => {
   const [activeTab, setActiveTab] = useState("1");
@@ -13,7 +14,17 @@ const Hr = () => {
     <>
       <NavBar
         title="HR"
-        pagetitle={activeTab === "1" ? "Employee" : activeTab === "2" ?"Attendance" : activeTab === "3" ? "Leave" : "Payroll"}
+        pagetitle={
+          activeTab === "1"
+            ? "Employee"
+            : activeTab === "2"
+            ? "Attendance"
+            : activeTab === "3"
+            ? "Leave"
+            : activeTab === "4"
+            ? "WFH"
+            : "Payroll"
+        }
       />
       <div className="cursor-pointer flex justify-between items-center ">
         <div className="font-layout-font flex gap-2  py-2 dark:text-white">
@@ -23,7 +34,7 @@ const Hr = () => {
             }`}
             onClick={() => setActiveTab("1")}
           >
-           Employee
+            Employee
           </p>
           <p
             className={`flex  gap-2  items-center  px-4 py-3 font-semibold rounded-sm text-sm ${
@@ -31,7 +42,7 @@ const Hr = () => {
             }`}
             onClick={() => setActiveTab("2")}
           >
-         Attendance
+            Attendance
           </p>
           <p
             className={`flex  gap-2  items-center  px-4 py-3 font-semibold rounded-sm text-sm ${
@@ -39,7 +50,7 @@ const Hr = () => {
             }`}
             onClick={() => setActiveTab("3")}
           >
-         Leave
+            Leave
           </p>
           <p
             className={`flex  gap-2  items-center  px-4 py-3 font-semibold rounded-sm text-sm ${
@@ -47,9 +58,15 @@ const Hr = () => {
             }`}
             onClick={() => setActiveTab("4")}
           >
-         Payroll
+            WFH
+          </p> <p
+            className={`flex  gap-2  items-center  px-4 py-3 font-semibold rounded-sm text-sm ${
+              activeTab === "5" ? "dark:bg-layout-dark bg-layout-light" : ""
+            }`}
+            onClick={() => setActiveTab("5")}
+          >
+            Payroll
           </p>
-         
         </div>
       </div>
       {activeTab === "1" ? (
@@ -58,10 +75,11 @@ const Hr = () => {
         <Attendance />
       ) : activeTab === "3" ? (
         <Leave />
+      ) : activeTab === "4" ? (
+        <WFH/>
       ) : (
         <PayRoll />
       )}
-      
     </>
   );
 };
